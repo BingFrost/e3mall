d63ef750158a0610818304c628f78b784bbf48e0
BingFrost <BingFrost@users.noreply.github.com>
2017-11-09 21:47:06 +0800

    增加商品分类tree

--- e3_manner/e3_manager_interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3_manner/e3_manager_interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,8 +1,10 @@
 package cn.e3mall.service;
 
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 
 	TbItem getItemById(long id);
+	DataGridResult getItemByPage(Integer page , Integer rows);
 }
